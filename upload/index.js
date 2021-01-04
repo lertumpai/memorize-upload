b759892c31beebe67ca19a08d8e613c4d85e5313
ninew <new-sorawit@outlook.com>
2021-01-04 15:31:01 +0700

    upload image

--- upload/index.js
+++ upload/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const fs = require('fs')
 const multer = require('multer')
+const sharp = require('sharp')
 
 const router = express.Router()
 
@@ -16,22 +17,58 @@ function generateString(n) {
 
 const profileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, './public/profile')
+    cb(null, './public/profiles')
   },
   filename: (req, file, cb) => {
     const ext = file.mimetype.split('/')[1]
-    cb(null, `${generateString(15)}-${Date.now()}.${ext}`)
+    cb(null, `${generateString(20)}-${Date.now()}.${ext}`)
   }
 })
 
 // const upload = multer({ storage: profileStorage })
 const uploadProfile = multer({ storage: profileStorage })
 
-router.post('/profile', uploadProfile.single('photo'), async (req, res) => {
+async function renameFile(req) {
   const { file } = req
+
   const fileName = `${req.body.userId}-${file.filename}`
-  await fs.renameSync(req.file.path, `${file.destination}/${fileName}`)
-  res.send({ urlImage: `http://localhost:4000/profile/${fileName}`})
+  const newFileName = `${file.destination}/${fileName}`
+  const fileName800 = `${req.body.userId}-800-${file.filename}`
+  const newFileName800 = `${file.destination}/${fileName800}`
+
+  // rename
+  await fs.renameSync(req.file.path, newFileName)
+
+  // resize
+  await sharp(newFileName).resize(800).toFile(newFileName800)
+
+  // remove original file
+  await fs.unlinkSync(newFileName)
+
+  return fileName800
+}
+
+router.post('/profile', uploadProfile.single('photo'), async (req, res) => {
+  const fileName = await renameFile(req)
+  res.send({ urlImage: `http://localhost:4000/profiles/${fileName}`})
+})
+
+const articleStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './public/articles')
+  },
+  filename: (req, file, cb) => {
+    const ext = file.mimetype.split('/')[1]
+    cb(null, `${generateString(20)}-${Date.now()}.${ext}`)
+  }
+})
+
+// const upload = multer({ storage: profileStorage })
+const uploadArticle = multer({ storage: articleStorage })
+
+router.post('/article', uploadArticle.single('photo'), async (req, res) => {
+  const fileName = await renameFile(req)
+  res.send({ urlImage: `http://localhost:4000/articles/${fileName}`})
 })
 
 module.exports = router
